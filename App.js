0b06d47b25d379c3a720f0a91690963f5c8d6fd2
Baran Dogan <phscloq@gmail.com>
2023-09-26 10:11:58 +0300

    UPDATE deleted not used codes

--- App.js
+++ App.js
@@ -1,14 +1,11 @@
 
-import { useState } from 'react';
 
 import { NavigationContainer } from '@react-navigation/native';
 import TabNavigator from './components/TabNavigator';
 export default function App() {
-const [count, setCount] = useState(0);
 
 return (
   <NavigationContainer> 
-{/* ... other components */}
 
 <TabNavigator />
 </NavigationContainer>
